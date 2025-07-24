3bda185539955b8dc7dd1b9d72f5d63d20db6e61
Joseph Ryan-Palmer <JosephRPalmer@users.noreply.github.com>
2025-07-24 21:24:26 +0100

    Add better error checking and correction, and timeout (#28)
    * Add better error checking and correction, and timeout
    
    * Move code back inside try except
    
    * Add better exponential backoff
    
    * change backoff lib
    
    * adjust timeouts

--- app/octopus_api_connection.py
+++ app/octopus_api_connection.py
@@ -4,11 +4,16 @@ from datetime import datetime, timedelta
 from jose import jwt
 from gql import Client, gql
 from gql.transport.requests import RequestsHTTPTransport, log as requests_logger
+from gql.transport.exceptions import TransportQueryError
+from urllib3.exceptions import ResponseError
 import requests
+import backoff
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(message)s')
+logging.getLogger('backoff').addHandler(logging.StreamHandler())
 logging.getLogger("requests.packages.urllib3").setLevel(logging.WARNING)
+logging.getLogger('backoff').setLevel(logging.WARNING)
 requests_logger.setLevel(logging.WARNING)
 
 class octopus_api_connection(BaseModel):
@@ -29,24 +34,28 @@ class octopus_api_connection(BaseModel):
                 url=self.api_url,
                 headers=self.headers,
                 verify=True,
-                retries=3
-            ),
+                retries=2,
+                timeout=20),
             fetch_schema_from_transport=False
         )
-        self.get_jwt(self.api_key)
+        self.get_jwt()
 
-    def check_jwt(self, api_key):
+    def check_jwt(self):
+        if self.headers.get("Authorization") is None:
+            logging.info("No JWT found, fetching new one")
+            self.get_jwt()
+            return
         try:
             user_info = jwt.decode(self.headers["Authorization"].split(" ")[1], key=self.key , algorithms=["RS256"])
             if (datetime.fromtimestamp(user_info["exp"]) > datetime.now() + timedelta(minutes=2)):
-                logging.info("JWT valid until {}".format(datetime.fromtimestamp(user_info["exp"])))
+                logging.debug("JWT valid until {}".format(datetime.fromtimestamp(user_info["exp"])))
             else:
-                self.get_jwt(api_key)
+                self.get_jwt()
         except (jwt.ExpiredSignatureError, jwt.JWTError) as e:
             logging.error("Hit error {} - {}, refreshing JWT".format(e.__class__.__name__, e))
-            self.get_jwt(api_key)
+            self.get_jwt()
 
-    def get_jwt(self, api_key):
+    def get_jwt(self):
         logging.info("Dropping headers")
         self.headers.clear()
         if not bool(self.headers):
@@ -60,7 +69,7 @@ class octopus_api_connection(BaseModel):
                 }
             }
         """)
-        jwt_query = self.client.execute(query, variable_values={"apiKey": api_key})
+        jwt_query = self.run_query(query, variable_values={"apiKey": self.api_key})
 
         self.headers["Authorization"] = "JWT {}".format(jwt_query['obtainKrakenToken']['token'])
 
@@ -68,6 +77,25 @@ class octopus_api_connection(BaseModel):
         return "jwt_query['obtainKrakenToken']['token']"
 
     def get_client(self):
-        self.check_jwt(self.api_key)
+        self.check_jwt()
 
         return self.client
+
+    def execute(self, query, variable_values=None):
+        self.check_jwt()
+        return self.run_query(query, variable_values)
+
+    @backoff.on_exception(backoff.expo,(TransportQueryError, ResponseError, requests.exceptions.ConnectionError), max_tries=5, jitter=backoff.full_jitter)
+    def run_query(self, query, variable_values=None):
+        try:
+            return self.client.execute(query, variable_values=variable_values)
+        except TransportQueryError as e:
+            logging.error("Possible rate limit hit, increase call interval")
+            logging.error(e)
+            raise  # Raise to trigger retry
+        except ResponseError as e:
+            logging.error("Response error: {}".format(e))
+            raise  # Raise to trigger retry
+        except Exception as e:
+            logging.error("Error executing query: {}".format(e))
+            raise  # Raise to trigger retry
